c2e83c5e5b268f6ccf06dbdbc4a048c200ca1b6a
McKayla Lankau <lankaukk@yahoo.com>
2021-08-10 15:08:15 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(input) {
+  const time = input.split(":");
+  const x = parseInt(time);
+
+  if (x < 12) {
+    return `Good Morning`;
+  }
+  else if (12 < x && x < 17) {
+    return `Good Afternoon`;
+  }
+  else if (x >= 17) {
+    return `Good Evening`;
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  const greeting = document.getElementById('greeting');
+  greeting.innerHTML = string;
+}
